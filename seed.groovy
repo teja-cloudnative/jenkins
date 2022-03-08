c1cd269e7de5491fe457bc0e0e87e0a800e05dcd
tejap <btejapramod@gmail.com>
2022-03-08 15:03:04 +0530

    New Changes

--- seed.groovy
+++ seed.groovy
@@ -3,29 +3,52 @@ folder('Terraform') {
     description('Terraform')
 }
 
-def jobs= [
-        [name : "VPC", git : "terraform-vpc"],
-        [name : "DB", git : "terraform-databases" ]
-]
 
-jobs.each {
-    def newmap = it;
-    //println("    ${it.name}: ${it.git}");
-    x = it.git
-    freeStyleJob("Terraform/${it.name}") {
-        scm {
-            git {
-                remote {
-                    name('origin')
-                    url("https://github.com/raghudevopsb62/${x}.git")
-                }
-                branches('*/main')
+//jobs.each {
+//    def newmap = it;
+//    //println("    ${it.name}: ${it.git}");
+//    x = it.git
+//    freeStyleJob("Terraform/${it.name}") {
+//        scm {
+//            git {
+//                remote {
+//                    name('origin')
+//                    url("https://github.com/raghudevopsb62/${x}.git")
+//                }
+//                branches('*/main')
+//            }
+//        }
+//
+//        steps {
+//            shell('make')
+//        }
+//
+//    }
+//}
+
+//def jobs= [
+//        [name : "VPC", git : "terraform-vpc"],
+//        [name : "DB", git : "terraform-databases" ]
+//]
+//
+//jobs.each {
+//    def newmap = it;
+//    //println("    ${it.name}: ${it.git}");
+//    x = it.git
+//}
+
+pipelineJob('github-demo') {
+   definition {
+     cpsScm {
+       scm {
+         git {
+            remote {
+               url('https://github.com/teja-cloudnative/jenkins.git')
+               }
+
+               }
             }
+         scriptPath('Jenkinsfile')
         }
-
-        steps {
-            shell('make')
-        }
-
     }
 }
